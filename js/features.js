ac03e35a498f50b04a0295cf50e89abdfd20da91
NureAlam68 <nurealam151068@gmail.com>
2024-09-25 01:54:12 +0600

    donation and history button function added

---
+++ js/features.js
@@ -0,0 +1,23 @@
+const donationBtn = document.getElementById('donation-btn');
+const historyBtn = document.getElementById('history-btn');
+
+// for change section
+
+donationBtn.addEventListener('click', function() {
+    showSectionById('donation-section');
+
+    donationBtn.classList.remove('bg-white');
+    donationBtn.classList.add('bg-color-one');
+    historyBtn.classList.remove('bg-color-one');
+    historyBtn.classList.add('bg-white');
+});
+
+
+historyBtn.addEventListener('click', function() {
+    showSectionById('history-section');
+
+    historyBtn.classList.remove('bg-white');
+    historyBtn.classList.add('bg-color-one');
+    donationBtn.classList.remove('bg-color-one');
+    donationBtn.classList.add('bg-white');
+});
---
+++ js/utilities.js
@@ -0,0 +1,9 @@
+
+// function for change section
+
+function showSectionById(id) {
+    document.getElementById('donation-section').classList.add('hidden');
+    document.getElementById('history-section').classList.add('hidden');
+
+    document.getElementById(id).classList.remove('hidden')
+}
