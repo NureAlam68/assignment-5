f168bb0ebf27f09c09a09563150304faf33a1a2a
NureAlam68 <nurealam151068@gmail.com>
2024-09-25 10:18:30 +0600

    added home.js file

--- js/blog.js
+++ js/blog.js
@@ -2,10 +2,12 @@
 
 
 
-document.getElementById('home-btn').addEventListener('click', function() {
-    window.location.href = './index.html'
-});
 
+
+
+document.getElementById('blog-btn').addEventListener('click', function() {
+    window.location.href = "blog.html"
+});
 
 
 
--- js/home.js
+++ js/home.js
@@ -1,5 +1,7 @@
 
 
-document.getElementById('blog-btn').addEventListener('click', function() {
-    window.location.href = './blog.html'
+
+
+document.getElementById('home-btn').addEventListener('click', function() {
+    window.location.href = "index.html"
 });
